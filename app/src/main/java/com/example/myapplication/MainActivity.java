c700336c4b4cbeaf7ac55102c2bcfbc7caf51e80
ITPS <ITPS@gmail.com>
2022-03-07 15:49:09 +0900

    layout 설정;

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.models.YtsData;
+import com.example.myapplication.repository.MovieService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1. viewBind 사용하기
+    private ActivityMainBinding binding;
+    private static final String TAG = MainActivity.class.getName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 뷰 바인딩 설정 방법 (Activity)
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        intData();
+    }
+
+    private void intData() {
+
     }
 }
